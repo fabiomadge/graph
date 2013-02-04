789403ccd217284a1d58957b330fda40fc119500
Fabio Madge-Pimentel <fabio@madge.me>
2013-02-04 20:15:38 +0100

    sceleton functions - snd try

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,7 @@
-import java.io.*;
-
 public class Main
 {
-	private static int[] a;
-	private static int[] b;
-	private static String input;
-
-
 	public static void main(String[] s) throws Exception{
-		try{
-			FileInputStream fstream = new FileInputStream("teams");
-			DataInputStream in = new DataInputStream(fstream);
-			while (in.available() !=0){
-				input = in.readLine();
-			}
-		in.close();
-		} 
-		catch (Exception e){
-			System.err.println("File input error");
-		}
+		Graph g = new Graph();
+		System.out.println(g);
 	}
 }
